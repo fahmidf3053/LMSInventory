1eb070d6d332cd7e45b8c8c431be21fbffc8a49b
Fahmid Ahmed <fahmidahmed16@gmail.com>
2023-05-12 03:32:01 +0600

    all api added

---
+++ LMSInventory/API/LMS.API/Controllers/ElementController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+
+using LMS.API.BusinessLogicLayer.Interfaces;
+using LMS.API.DTOs.RequestDTOs;
+using LMS.API.Exceptions;
+
+namespace LMS.API.Controllers
+{
+    [Authorize]
+    [Route("lms/api")]
+    [ApiController]
+    public class ElementController : ControllerBase
+    {
+        private readonly IElementManager _elementManager;
+
+        public ElementController(IElementManager elementManager)
+        {
+            _elementManager = elementManager;
+        }
+
+        [HttpPost("getAllElements")]
+        public IActionResult GetAllElements(int pageSize, int pageNumber)
+        {
+            try
+            {
+                var elements = _elementManager.GetAllElements(pageSize, pageNumber);
+
+                return Ok(elements);
+            }
+            catch (ElementException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                ElementException ex = new(ElementExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("getElementById")]
+        public IActionResult GetElementById([Required] int id)
+        {
+            try
+            {
+                var elements = _elementManager.GetElementById(id);
+
+                return Ok(elements);
+            }
+            catch (ElementException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                ElementException ex = new(ElementExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("saveElement")]
+        public IActionResult SaveElement([Required] ElementReqDTO reqDTO)
+        {
+            try
+            {
+                var element = _elementManager.SaveElement(reqDTO);
+
+                return Ok(element);
+            }
+            catch (ElementException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                ElementException ex = new(ElementExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("editElement")]
+        public IActionResult EditElement([Required] ElementReqDTO reqDTO)
+        {
+            try
+            {
+                var element = _elementManager.EditElement(reqDTO);
+
+                return Ok(element);
+            }
+            catch (ElementException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                ElementException ex = new(ElementExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("deleteElement")]
+        public IActionResult DeleteElement([Required] int id)
+        {
+            try
+            {
+                var response = _elementManager.DeleteElement(id);
+
+                return Ok(response);
+            }
+            catch (ElementException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                ElementException ex = new(ElementExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+    }
+}
---
+++ LMSInventory/API/LMS.API/Controllers/RackController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+
+using LMS.API.BusinessLogicLayer.Interfaces;
+using LMS.API.DTOs.RequestDTOs;
+using LMS.API.Exceptions;
+
+namespace LMS.API.Controllers
+{
+    [Authorize]
+    [Route("lms/api")]
+    [ApiController]
+    public class RackController : ControllerBase
+    {
+        private readonly IRackManager _rackManager;
+
+        public RackController(IRackManager rackManager)
+        {
+            _rackManager = rackManager;
+        }
+
+        [HttpPost("getAllRacks")]
+        public IActionResult GetAllRacks(int pageSize, int pageNumber)
+        {
+            try
+            {
+                var racks = _rackManager.GetAllRacks(pageSize, pageNumber);
+
+                return Ok(racks);
+            }
+            catch (RackException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                RackException ex = new(RackExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("getRackById")]
+        public IActionResult GetRackById([Required] int id)
+        {
+            try
+            {
+                var racks = _rackManager.GetRackById(id);
+
+                return Ok(racks);
+            }
+            catch (RackException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                RackException ex = new(RackExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("saveRack")]
+        public IActionResult SaveRack([Required] RackReqDTO reqDTO)
+        {
+            try
+            {
+                var rack = _rackManager.SaveRack(reqDTO);
+
+                return Ok(rack);
+            }
+            catch (RackException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                RackException ex = new(RackExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("editRack")]
+        public IActionResult EditRack([Required] RackReqDTO reqDTO)
+        {
+            try
+            {
+                var rack = _rackManager.EditRack(reqDTO);
+
+                return Ok(rack);
+            }
+            catch (RackException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                RackException ex = new(RackExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+
+        [HttpPost("deleteRack")]
+        public IActionResult DeleteRack([Required] int id)
+        {
+            try
+            {
+                var response = _rackManager.DeleteRack(id);
+
+                return Ok(response);
+            }
+            catch (RackException ex)
+            {
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+            catch (Exception e)
+            {
+                RackException ex = new(RackExceptions.UnhandledError);
+
+                return Problem(detail: ex.StatusMessage, statusCode: ex.StatusCode, title: ex.Title);
+            }
+        }
+    }
+}
