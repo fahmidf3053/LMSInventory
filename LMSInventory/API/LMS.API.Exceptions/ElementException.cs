d6b98df822e8ef1014df967b6ffb0c43188916d9
Fahmid Ahmed <fahmidahmed16@gmail.com>
2023-05-12 03:01:41 +0600

    all exceptions added

---
+++ LMSInventory/API/LMS.API.Exceptions/ElementException.cs
@@ -0,0 +1,44 @@
+﻿namespace LMS.API.Exceptions
+{
+    public class ElementException : Exception
+    {
+        public ElementException(string errorType)
+        {
+            StatusCode = int.Parse(ErrorDictionary[errorType][0]);
+            Title = ErrorDictionary[errorType][1];
+            StatusMessage = ErrorDictionary[errorType][2];
+        }
+
+        public int StatusCode { get; set; }
+        public string Title { get; set; }
+        public string StatusMessage { get; set; }
+
+        private static readonly Dictionary<string, List<string>> ErrorDictionary = new()
+        {
+            {
+                "ElementInfoNotFound", new List<string>(){"404", "Not Found", "Element Info Not Found." }
+            },
+            {
+                "ElementSaveFailed", new List<string>(){"400", "Bad Request", "Couldn't Save the Element." }
+            },
+            {
+                "WrongElementInfo", new List<string>(){"400", "Bad Request", "Wrong Element Info." }
+            },
+            {
+                "ElementDeleteFailed", new List<string>(){"400", "Bad Request", "Couldn't Delete the Element." }
+            },
+            {
+                "UnhandledError", new List<string>(){"500", "Server Error", "Internal Server Error." }
+            }
+        };
+
+    }
+    public static class ElementExceptions
+    {
+        public const string ElementInfoNotFound = "ElementInfoNotFound";
+        public const string ElementSaveFailed = "ElementSaveFailed";
+        public const string WrongElementInfo = "WrongElementInfo";
+        public const string ElementDeleteFailed = "ElementDeleteFailed";
+        public const string UnhandledError = "UnhandledError";
+    }
+}
---
+++ LMSInventory/API/LMS.API.Exceptions/RackException.cs
@@ -0,0 +1,44 @@
+﻿namespace LMS.API.Exceptions
+{
+    public class RackException : Exception
+    {
+        public RackException(string errorType)
+        {
+            StatusCode = int.Parse(ErrorDictionary[errorType][0]);
+            Title = ErrorDictionary[errorType][1];
+            StatusMessage = ErrorDictionary[errorType][2];
+        }
+
+        public int StatusCode { get; set; }
+        public string Title { get; set; }
+        public string StatusMessage { get; set; }
+
+        private static readonly Dictionary<string, List<string>> ErrorDictionary = new()
+        {
+            {
+                "RackInfoNotFound", new List<string>(){"404", "Not Found", "Rack Info Not Found." }
+            },
+            {
+                "RackSaveFailed", new List<string>(){"400", "Bad Request", "Couldn't Save the Rack." }
+            },
+            {
+                "WrongRackInfo", new List<string>(){"400", "Bad Request", "Wrong Rack Info." }
+            },
+            {
+                "RackDeleteFailed", new List<string>(){"400", "Bad Request", "Couldn't Delete the Rack." }
+            },
+            {
+                "UnhandledError", new List<string>(){"500", "Server Error", "Internal Server Error." }
+            }
+        };
+
+    }
+    public static class RackExceptions
+    {
+        public const string RackInfoNotFound = "RackInfoNotFound";
+        public const string RackSaveFailed = "RackSaveFailed";
+        public const string WrongRackInfo = "WrongRackInfo";
+        public const string RackDeleteFailed = "RackDeleteFailed";
+        public const string UnhandledError = "UnhandledError";
+    }
+}
